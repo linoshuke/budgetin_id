1cf5a1ee5bd72cc3955430ec7b9efefec221630a
Lino <Dedeichsan.r15@gmail.com>
2025-10-11 09:49:18 +0700

    Add Google GenAI and related dependencies
    Added @google/genai and its required dependencies to package.json and package-lock.json. Updated google-services.json with a new Android client and made minor formatting changes to build.gradle.kts.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,9 +31,9 @@ android {
 
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
